a6402ff681ebcc891076fa28101e6c01c2735458
shiyas <shiyasrasimdx@gmail.com>
2025-11-07 18:56:45 +0400

    added methods

--- AutomationWebtesting/src/test/java/openwebpage.java
+++ AutomationWebtesting/src/test/java/openwebpage.java
@@ -1,10 +1,14 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.Reporter;
 import org.testng.annotations.*;
+
+import java.sql.Driver;
+import java.util.List;
 
 public class openwebpage {
     WebDriver driver;
@@ -54,9 +58,53 @@ public class openwebpage {
         driver.findElement(By.cssSelector("#currentAddress")).sendKeys("Test CurrentAddress\nteststreet\ntestpostcode");
         driver.findElement(By.cssSelector("#permanentAddress")).sendKeys("Test Permanent Address\nteststreet\ntestpostcode");
 
-        js.executeScript("window.scrollBy(0, 400)");
+        js.executeScript("window.scrollBy(0, 300)");
         driver.findElement(By.cssSelector("#submit")).click();
         Reporter.log("✅ Test Passed: Clicked on tab!");
+    }
+
+    @Test(dependsOnMethods = "M4_EnterDetailstoETab")
+    public void M5_CheckBoxclick() {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        //js.executeScript("window.scroll(0,-500 )");
+        driver.findElement(By.cssSelector("#item-1")).click();
+        Assert.assertTrue(driver.getCurrentUrl().equals("https://demoqa.com/checkbox"), "❌ Click did not navigate to the Elements-->CheckBox page!");
+
+        Reporter.log("✅ Test Passed: Clicked on CheckBox!");
+        js.executeScript("window.scroll(0,200 )");
+        boolean Cflag = false;
+//        try {
+//            Thread.sleep(5000); // 2 seconds
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        WebElement checkbox = driver.findElement(By.xpath("//span[@class='rct-checkbox']"));
+        //driver.findElement(By.xpath("//span[@class='rct-checkbox']")).click();
+
+
+        if (!checkbox.isSelected()) {
+            checkbox.click();
+            Cflag = true;
+        }
+        Assert.assertTrue(Cflag,"User is not able to click CheckBox!");
+    }
+    @Test(dependsOnMethods = "M5_CheckBoxclick")
+    public void M6_PrinttheListofcheckboxes() {
+        driver.findElement(By.xpath("//button[@title='Expand all']")).click();
+        List<WebElement> checkboxes = driver.findElements(By.cssSelector("span.rct-checkbox"));
+        for (WebElement checkbox : checkboxes)
+            {
+                WebElement label = checkbox.findElement(By.xpath("./following-sibling::span[@class='rct-title']"));
+            System.out.println(label.getText());
+            }
+//        for (WebElement checkbox : checkboxes) {
+//            WebElement input = checkbox.findElement(By.xpath("./ancestor::label/input"));
+//            if (!input.isSelected()) {
+//                checkbox.click();
+//                Assert.assertTrue(input.isSelected());
+//            }
+//        }
+
     }
     @AfterClass
     public void tearDown() {
